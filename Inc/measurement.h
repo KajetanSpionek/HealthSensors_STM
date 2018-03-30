354d963e18ff8ee6d3850b6e9e85c09937323ad5
Kajetan Śpionek <kajetanspionek@gmail.com>
2018-03-30 20:34:35 +0200

    Basic measurementInfo structure and Setting measurement session

--- Inc/measurement.h
+++ Inc/measurement.h
@@ -10,4 +10,43 @@
 
 #include "stm32l1xx_hal.h"
 
+typedef struct {
+
+	uint8_t is_active; 			/* If device is in measurement mode
+	 	 	 	 	 	 	 	 1 - Measurement mode
+	 	 	 	 	 	 	 	 0 - Idle */
+	uint8_t mode;				/* Device mode:
+							 	 0 - Passive (wifi connection not required)
+							 	 1 - Active (wifi connection required)
+							 	 2 - Single sweep */
+	uint8_t type;				/* Measurement type:
+							 	 1 - ECG
+							 	 2 - PPG
+							 	 3 - Both */
+	uint8_t freq; 				/* Frequency of measurements in minutes */
+	uint8_t duration; 			/* Duration of measurement in seconds */
+	uint8_t length; 			/* Length of measurements in hours */
+	uint8_t start_time[3];   	/* Time of first measurement (H/M/S) */
+	uint8_t left_measurements;  /* Number of left measurements in this session */
+	uint8_t next_time[3];		/* Time of next measurement (H/M/S) */
+
+} MeasurementStruct;
+
+MeasurementStruct MeasurementInfo;
+
+/**
+   * @brief Measurements session initialization
+   * @param mode:
+   * @param type:
+   * @param freq:
+   * @param duration:
+   * @param length:
+   * @param start_time:
+   * @retval status: 	0 - Measurements session started correctly
+   * 					1 - Session already in progress
+*/
+uint8_t MEASUREMENT_setMeasurement(uint8_t mode, uint8_t type, uint8_t freq, uint8_t duration,
+									uint8_t length, uint8_t* start_time);
+
+
 #endif /* MEASUREMENT_H_ */
--- Src/measurement.c
+++ Src/measurement.c
@@ -6,3 +6,34 @@
  */
 
 #include "measurement.h"
+
+uint8_t MEASUREMENT_setMeasurement(uint8_t mode, uint8_t type, uint8_t freq, uint8_t duration,
+									uint8_t length, uint8_t* start_time) {
+
+	// Check if measurement session in progress
+	if (MeasurementInfo.is_active == 1) return 1;
+
+	// Set basic measurement parameters
+	MeasurementInfo.mode = mode;
+	MeasurementInfo.type = type;
+	MeasurementInfo.freq = freq;
+	MeasurementInfo.duration = duration;
+	MeasurementInfo.length = length;
+	MeasurementInfo.start_time[0] = start_time[0];
+	MeasurementInfo.start_time[1] = start_time[1];
+	MeasurementInfo.start_time[2] = start_time[2];
+
+	// Calculate left_measurements
+	MeasurementInfo.left_measurements = length * 60 / freq;
+
+	// Set next measurement to starting time
+	MeasurementInfo.next_time[0] = start_time[0];
+	MeasurementInfo.next_time[1] = start_time[1];
+	MeasurementInfo.next_time[2] = start_time[2];
+
+	// Set Alarm
+
+
+	// Set is_active flag
+	MeasurementInfo.is_active = 1;
+}
